a37ee28986952253e425392550c9f744f85e0e0a
Harold <haroldhong.love@gmail.com>
2021-03-25 20:48:27 -0500

    init helpers

---
+++ src/helpers/styles/colors.tsx
@@ -0,0 +1,22 @@
+const color = {
+  text: {
+    primary: '#1E1E1E',
+    secondary: '#8894AC',
+    danger: '#FF0000',
+    light: '#ffffff',
+    black: '#000',
+  },
+  bg: {
+    primary: '#1E1E1E',
+    secondary: '#EF405B',
+    light: '#FFFFFF',
+    black: '#000',
+    danger: '#FF0000',
+  },
+  accent: {
+    1: '#F7F8FA',
+    2: '#C8CDD7',
+  },
+}
+
+export default color
---
+++ src/helpers/styles/index.tsx
@@ -0,0 +1,6 @@
+import color from './colors'
+import { spaceMb, spaceDt } from './spacers'
+import { screenMin, screenMax, breakpoints } from './breakpoints'
+import { getTypography, getFontSet } from './typography'
+
+export { screenMin, screenMax, breakpoints, color, spaceMb, spaceDt, getTypography, getFontSet }
