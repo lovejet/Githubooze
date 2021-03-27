47b56069455002b0ccc2e0641232f6ef824372c3
Harold <haroldhong.love@gmail.com>
2021-03-27 05:44:12 -0500

    init user card

--- src/components/fragments/GithubUserCard/GithubUserCard.styled.tsx
+++ src/components/fragments/GithubUserCard/GithubUserCard.styled.tsx
@@ -3,10 +3,49 @@ import { color, spaceDt } from '@helpers/styles'
 import styled from 'styled-components'
 
 const GithubUserCardContainer = styled.div`
+  display: flex;
+  flex-direction: row;
   width: ${toVW(420, 'desktop')};
   height: ${toVW(200, 'desktop')};
   margin: ${spaceDt(2)};
   background-color: ${color.bg.primary};
 `
 
-export { GithubUserCardContainer }
+const AvatarPane = styled.div`
+  width: ${toVW(200, 'desktop')};
+  height: ${toVW(200, 'desktop')};
+  padding: ${spaceDt(2)};
+`
+
+const Avatar = styled.img`
+  width: 100%;
+  height: 100%;
+`
+
+const InfoPane = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: calc(100% - ${toVW(200, 'desktop')} - ${spaceDt(2)});
+  margin: ${spaceDt(2)} 0;
+  justify-content: center;
+`
+
+const UserId = styled.div`
+  width: 100%;
+  text-align: center;
+  margin-bottom: ${spaceDt(2)};
+`
+
+const Links = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+`
+
+const Link = styled.a`
+  cursor: pointer;
+  color: ${color.bg.secondary};
+  text-decoration: none;
+`
+
+export { GithubUserCardContainer, AvatarPane, InfoPane, Avatar, UserId, Links, Link }
--- src/components/fragments/GithubUserCard/GithubUserCard.tsx
+++ src/components/fragments/GithubUserCard/GithubUserCard.tsx
@@ -1,11 +1,22 @@
 import { memo } from 'react'
-import { GithubUserCardContainer } from './GithubUserCard.styled'
+import { GithubUserCardContainer, AvatarPane, InfoPane, Avatar, UserId, Links, Link } from './GithubUserCard.styled'
 import { INTERFACE_USER } from '@helpers/types'
+import avatar from '@images/avatar.png'
 
 const GithubUserCard = ({ user }: { user: INTERFACE_USER}) => {
   return (
     <GithubUserCardContainer>
-      GithubUserCard
+      <AvatarPane>
+        {/* <Avatar src={user.avatar_url} /> */}
+        <Avatar src={avatar} />
+      </AvatarPane>
+      <InfoPane>
+        <UserId>{user.login}</UserId>
+        <Links>
+          <Link href={user.url}>Github</Link>
+          <Link>Detail</Link>
+        </Links>
+      </InfoPane>
     </GithubUserCardContainer>
   )
 }
